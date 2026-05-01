234733754bd6d7ebd6d1d1a766f67ea3e7e97142
lajilihadil <hadil.lajili@esprim.tn>
2026-05-02 00:28:15 +0100

    Amélioration modèle : recall 46% -> 66%

--- model_pipeline.py
+++ model_pipeline.py
@@ -35,10 +35,13 @@ def prepare_data(filepath):
     print("Données prêtes !")
     return X_train, X_test, y_train, y_test, scaler
 
-def train_model(X_train, y_train, n_estimators=100, random_state=42):
+def train_model(X_train, y_train, n_estimators=200, random_state=42):
     model = RandomForestClassifier(
         n_estimators=n_estimators,
-        random_state=random_state
+        random_state=random_state,
+        class_weight='balanced',
+        max_depth=10,
+        min_samples_split=5
     )
     model.fit(X_train, y_train)
     mlflow.log_param("n_estimators", n_estimators)
